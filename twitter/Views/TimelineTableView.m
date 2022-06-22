ede10c4fe3844412975e78023e06c4d3cf902057
Gael Rodriguez Gomez <gaelrg@fb.com>
2022-06-21 23:52:00 -0700

    First tweet cell model, missing buttons

---
+++ twitter/Views/TimelineTableView.m
@@ -0,0 +1,21 @@
+//
+//  TimelineTableView.m
+//  twitter
+//
+//  Created by Gael Rodriguez Gomez on 6/21/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "TimelineTableView.h"
+
+@implementation TimelineTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
